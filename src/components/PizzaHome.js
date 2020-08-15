7172d4d75ce76938d53294a0752b86ba12b18115
Jonathan Deltoro <jonathandeltoro@Jonathans-MacBook-Air.local>
2020-08-14 20:30:52 -0600

    initial commit, added dependencies, created form and home.

---
+++ src/components/PizzaHome.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../index.css';
+
+
+
+const PizzaHome = () => {
+    return (
+        <div className='PizzaHome'>
+            <h1>lambda pizza!</h1>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default PizzaHome; 
